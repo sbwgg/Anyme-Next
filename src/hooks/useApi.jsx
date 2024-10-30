12b24a64a2ac0e38188c0a038dfff6b8b1d1eabe
Berto <82095317+sbwgg@users.noreply.github.com>
2024-10-30 16:27:05 +0000

    urlfix

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -3,7 +3,7 @@ import { jaroWinklerDistance } from "./jaro-winkler";
 const PROXY = process.env.PROXY_URL;
 const apiLink = process.env.CONSUMET_URL;
 const BASE_URL = process.env.CONSUMET_URL + "/meta/anilist/";
-const ANIWATCH_URL = process.env.HIANIME_URL + "anime/";
+const ANIWATCH_URL = process.env.HIANIME_URL + "/anime/";
 const API_KEY = "e2f1fb12caa883224a8363dc0329b3bc";
 const BASE_MOVIE_URL = "https://api.themoviedb.org";
 const MANGA_URL = process.env.MANGA_URL + "api/";
